35ce4fb327d8dbbd86e2cbcc1a0525e81f77cca9
Luma <markermails@gmail.com>
2020-04-12 07:37:33 +0900

    Change gulp-clean to del; Integrate with EJS.

--- gulpfile.esm.js
+++ gulpfile.esm.js
@@ -1,15 +1,15 @@
-import { series, parallel, src, dest, watch } from "gulp";
+import { series, parallel, src, dest, watch as gulpWatch } from "gulp";
 const browserSync = require("browser-sync").create();
 const typescript = require("gulp-typescript");
 const sourcemaps = require("gulp-sourcemaps");
-const clean = require("gulp-clean");
+const gulpEjs = require("gulp-ejs");
+const rename = require('gulp-rename')
+const log = require('fancy-log')
+const del = require('del')
 const destination = "dist/";
 
-export function cleanAll(done) {
-  src(["dist/**/*.{js,css,html}"], {
-    read: false,
-  }).pipe(clean());
-  done();
+export function clean(done) {
+  return del([`${destination}**/*.{js,css,html}`]);
 }
 export function ts(done) {
   src("src/**/*.ts")
@@ -24,19 +24,29 @@ export function css(done) {
   done();
 }
 export function html(done) {
-  src("src/**/*.html").pipe(dest(destination));
+  src("src/**/*.html")
+    .pipe(dest(destination));
+  done();
+}
+export function ejs(done) {
+  src(["src/**/*.ejs", "!**/_*.ejs"])
+    .pipe(gulpEjs())
+    .on('error', log)
+    .pipe(rename({ extname: '.html' }))
+    .pipe(dest(destination))
   done();
 }
 export function reload(done) {
   browserSync.reload();
   done();
 }
-export const all = parallel(ts, css, html);
+export const all = parallel(ts, css, html, ejs);
 
-export function watchAll() {
-  watch("src/**/*.ts", { events: "all" }, series(ts, reload));
-  watch("src/**/*.css", { events: "all" }, series(css, reload));
-  watch("src/**/*.html", { events: "all" }, series(html, reload));
+export function watch() {
+  gulpWatch("src/**/*.ts", { events: "all" }, series(ts, reload));
+  gulpWatch("src/**/*.css", { events: "all" }, series(css, reload));
+  gulpWatch("src/**/*.html", { events: "all" }, series(html, reload));
+  gulpWatch("src/**/*.ejs", { events: "all" }, series(ejs, reload));
 }
 export function server(done) {
   browserSync.init({
@@ -48,5 +58,5 @@ export function server(done) {
   done();
 }
 
-export const build = series(cleanAll, all);
-export default series(cleanAll, all, server, watchAll);
+export const build = series(clean, all);
+export default series(clean, all, server, watch);
